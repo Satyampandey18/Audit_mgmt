50310dcd53e8bdc16e16c9f80a364c9c081983ea
Satyampandey18 <satyampandey0606@gmail.com>
2022-08-23 16:58:46 +0530

    first commit

---
+++ backend/AuditSeverity/AuditSeverity/Controllers/AuditSeverityController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AuditSeverityModule.Models;
+using AuditSeverityModule.Providers;
+using AuditSeverityModule.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuditSeverityModule.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuditSeverityController : ControllerBase
+    {
+        private readonly ISeverityProvider _severityProvider;
+        public readonly log4net.ILog _log4net = log4net.LogManager.GetLogger(typeof(AuditSeverityController));
+        public AuditSeverityController(ISeverityProvider severityProvider)
+        {
+            _severityProvider = severityProvider;
+        }
+
+        //[HttpGet]
+        //public IActionResult ProjectExecutionStatus()
+        //{
+        //    _log4net.Info(" Http GET Request From: " + nameof(AuditSeverityController));
+        //    return Ok("SUCCESS");
+        //}
+
+
+
+        [HttpPost]
+        public IActionResult ProjectExecutionStatus([FromBody] AuditRequest req)
+        {
+            _log4net.Info(" Http POST Request From: " + nameof(AuditSeverityController));
+
+            if (req == null)
+                return BadRequest();
+
+            if (req.Auditdetails.Type != "Internal" && req.Auditdetails.Type != "SOX")
+                return BadRequest("Wrong Audit Type");
+
+            try
+            {
+                AuditResponse response = _severityProvider.SeverityResponse(req);
+                return Ok(response);
+            }
+            catch (Exception e)
+            {
+                _log4net.Error(e.Message);
+                return StatusCode(500);
+            }
+
+        }
+
+
+    }
+}
---
+++ backend/AuthorizationService-1/Repository/CredentialsRepo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthorizationService.Repository
+{
+    public class CredentialsRepo: ICredentialsRepo
+    {
+        private Dictionary<string, string> ValidUsersDictionary = new Dictionary<string, string>()
+        {
+               {"satyam","satyam"},
+               {"shivam","shivam"}
+        };
+        public Dictionary<string,string> GetCredentials()
+        {
+            return ValidUsersDictionary;
+        }
+    }
+}
